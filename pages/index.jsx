26ca8b7a2443c6f6eb30062ea52dcb00cee9e96c
Mughees0 <abdul.mughees009@gmail.com>
2023-05-01 18:24:39 +0300

    Setup

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="bg-blue-400 ">
-      <div className="bg-white text-black">helsfdhkfslo world</div>
+    <main className="bg-blue-400 w-screen h-screen flex items-center justify-center">
+      <button className=" bg-white py-3 px-5 rounded-xl">Login with google</button>
     </main>
   );
 }
