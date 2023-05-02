5091f81bfc42b6f371c2ebc10639285417bd4dfa
Mughees0 <abdul.mughees009@gmail.com>
2023-05-02 06:00:26 +0300

    Admin page starting to come into shape

---
+++ components/Layout.jsx
@@ -0,0 +1,33 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/Nav";
+
+export default function Layout({children}) {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <main className="bg-blue-400 w-screen h-screen flex ">
+        <Nav />
+        <section className="bg-yellow-200 flex flex-grow my-2 mr-2 rounded-lg p-2">
+          {children}
+        </section>
+        {/* <button
+          className=" bg-white py-2 px-5 rounded-xl"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button> */}
+      </main>
+    );
+  }
+  return (
+    <main className="bg-blue-400 w-screen h-screen flex items-center justify-center">
+      <button
+        className=" bg-white py-3 px-5 rounded-xl"
+        onClick={() => signIn("google")}
+      >
+        Sign in with Google
+      </button>
+    </main>
+  );
+}
+
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,5 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <main className="bg-blue-400 w-screen h-screen flex items-start justify-end gap-5 pt-5 px-5">
-        Signed in as {session.user.email} <br />
-        <button
-          className=" bg-white py-3 px-5 rounded-xl"
-          onClick={() => signOut()}
-        >
-          Sign out
-        </button>
-      </main>
-    );
-  }
-  return (
-    <main className="bg-blue-400 w-screen h-screen flex items-center justify-center">
-      <button
-        className=" bg-white py-3 px-5 rounded-xl"
-        onClick={() => signIn("google")}
-      >
-        Sign in with Google
-      </button>
-    </main>
-  );
+  return <Layout>test</Layout>;
 }
-//
